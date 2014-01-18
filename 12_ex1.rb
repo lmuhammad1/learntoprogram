8da8541eb0a745a5911cf507a3252b1523cfea0e
Louis Muhammad <louis.muhammad@gmail.com>
2014-01-18 08:07:51 -0500

    Ch 12: Happy Birthday

--- 12_ex1.rb
+++ 12_ex1.rb
@@ -16,8 +16,10 @@ min = gets.chomp().to_i
 date_born = Time.new(year, month, day, hour, min)
 one_billion_seconds_old = date_born + 1000000000
 current_age_in_seconds = Time.new - date_born
+current_age = current_age_in_seconds / 60 / 60 / 24 / 365
 
 puts "You were born on #{date_born}."
 puts "You will turn one billion seconds on #{one_billion_seconds_old}!"
 puts "You are currently #{current_age_in_seconds} seconds old!"
-
+puts "You are currently #{current_age.to_i} and deserve a smack :) !"
+puts "SPANK!\n" * current_age.to_i
